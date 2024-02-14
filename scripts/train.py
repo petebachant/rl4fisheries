a15bd3c818716a2c46a3914b7990d8df9727a771
Carl <cboettig@gmail.com>
2024-02-14 03:23:39 +0000

    progress?

--- scripts/train.py
+++ scripts/train.py
@@ -4,5 +4,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="Path config file", type=str)
 args = parser.parse_args()
 
+from gymnasium.envs.registration import register
+
+# action is 'harvest'
+register(id="Asm-v0", entry_point="rl4fisheries.asm:Asm")
+# action is 'escapement'
+register(id="AsmEsc-v0", entry_point="rl4fisheries.asm_esc:AsmEsc")
+
 from rl4eco.utils import sb3_train    
 sb3_train(args.file)
