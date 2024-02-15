67d6f5eddafbbdb0846d398bcad027aba3cc9499
Carl Boettiger <cboettig@gmail.com>
2024-02-15 12:49:32 -0800

    Adds some minimal tests (#2)
    * some minimal tests
    
    * nox
    
    * keep it simple
    
    * not plural
    
    * skip codecov
    
    * whoops, revert ipynb edit

--- src/rl4fisheries/__init__.py
+++ src/rl4fisheries/__init__.py
@@ -1,7 +1,8 @@
-from .asm import Asm
-from .asm_2o import Asm2o
-from .cautionary_rule import CautionaryRule
-from .const_esc import ConstEsc
-from .msy import Msy
-from .unit_interface import unitInterface
+from rl4fisheries.envs.asm import Asm
+from rl4fisheries.envs.asm_2o import Asm2o
+from rl4fisheries.envs.asm_esc import AsmEsc
+from rl4fisheries.agents.cautionary_rule import CautionaryRule
+from rl4fisheries.agents.const_esc import ConstEsc
+from rl4fisheries.agents.msy import Msy
+from rl4fisheries.agents.unit_interface import unitInterface
 
--- tests/test_asm.py
+++ tests/test_asm.py
@@ -1,6 +1,12 @@
 # Confirm environment is correctly defined:
 from stable_baselines3.common.env_checker import check_env
+from rl4fisheries import Asm, Asm2o, AsmEsc
 
+def test_Asm():
+    check_env(Asm(), warn=True)
+
+def test_Asm2o():
+    check_env(Asm2o(), warn=True)
 
 def test_AsmEsc():
     check_env(AsmEsc(), warn=True)
