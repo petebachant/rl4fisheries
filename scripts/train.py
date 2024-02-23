e04029267b9261ca9dc831fdbffdf280bc5e8a24
Carl Boettiger <cboettig@gmail.com>
2024-02-23 14:08:34 -0800

    register gym env names (#5)

--- scripts/train.py
+++ scripts/train.py
@@ -4,14 +4,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="Path config file", type=str)
 args = parser.parse_args()
 
-from gymnasium.envs.registration import register
-
-# action is 'harvest'
-register(id="Asm-v0", entry_point="rl4fisheries.asm:Asm")
-# action is 'escapement'
-register(id="AsmEsc-v0", entry_point="rl4fisheries.asm_esc:AsmEsc")
-# action is harvest, but observes both total count and mean biomass
-register(id="Asm2o-v0", entry_point="rl4fisheries.asm_2o:Asm2o")
+import rl4fisheries
 
 
 from rl4eco.utils import sb3_train    
--- src/rl4fisheries/__init__.py
+++ src/rl4fisheries/__init__.py
@@ -6,3 +6,10 @@ from rl4fisheries.agents.const_esc import ConstEsc
 from rl4fisheries.agents.msy import Msy
 from rl4fisheries.agents.unit_interface import unitInterface
 
+from gymnasium.envs.registration import register
+# action is 'harvest'
+register(id="Asm-v0", entry_point="rl4fisheries.envs.asm:Asm")
+# action is 'escapement'
+register(id="AsmEsc-v0", entry_point="rl4fisheries.envs.asm_esc:AsmEsc")
+# action is harvest, but observes both total count and mean biomass
+register(id="Asm2o-v0", entry_point="rl4fisheries.envs.asm_2o:Asm2o")
