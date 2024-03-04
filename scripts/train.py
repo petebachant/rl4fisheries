bd9d1b5850c18059b828c9d0b5d4a44756a159b5
Felipe Montealegre-Mora <felimomouni@gmail.com>
2024-03-04 22:05:40 +0000

    progress bar option

--- scripts/train.py
+++ scripts/train.py
@@ -2,6 +2,7 @@
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="Path config file", type=str)
+parser.add_argument("-pb", "--progress_bar", help="Use  progress bar for training", type=bool)
 args = parser.parse_args()
 
 import rl4fisheries
