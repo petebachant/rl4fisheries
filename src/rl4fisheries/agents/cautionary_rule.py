ba789ec520466908d3153780cc3b5f25fb4497d4
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-04-24 01:57:12 +0000

    cautionary rule now has two possible obserrvations

--- src/rl4fisheries/agents/cautionary_rule.py
+++ src/rl4fisheries/agents/cautionary_rule.py
@@ -9,31 +9,49 @@ from .unit_interface import unitInterface
 from rl4fisheries.agents.common import isVecObs
 
 class CautionaryRule:
-    def __init__(self, env, x1=0, x2=1, y2=1, obs_bounds=1, **kwargs):
-        self.ui = unitInterface(bounds=obs_bounds)
+    def __init__(self, env, x1=0, x2=1, y2=1, observed_var='biomass', **kwargs):
+        self.policy_type = f"CautionaryRule_{observed_var}"
+        self.env = env
+        self.observed_var = observed_var
+        
         self.x1 = x1
         self.x2 = x2
         self.y2 = y2
-        self.policy_type = "CautionaryRule_piecewise_linear"
-        self.env = env
+
+        self.x1_pm1 = self.convert_to_pm1(x1) 
+        self.x2_pm1 = self.convert_to_pm1(x2)
+        self.y_pm1 = self.convert_to_pm1(y)
 
         assert x1 <= x2, "CautionaryRule error: x1 <= x2" 
 
+    def convert_to_pm1(self, X):
+        if self.observed_var == 'biomass':
+            return self.env.bound * (X+1)/2
+        elif self.observed_var == 'mean_wt':
+            MAX_WT = self.env.parameters["max_wt"]
+            MIN_WT = self.env.parameters["min_wt"]
+            return MIN_WT + ((X+1) / 2) * (MAX_WT - MIN_WT)
+    
     def predict(self, observation, **kwargs):
         if isVecObs(observation, self.env):
             observation = observation[0]
-        pop = self.ui.to_natural_units(observation)
-        raw_prediction = np.clip( self.predict_raw(pop), 0, 1)
-        return np.float32([2 * raw_prediction - 1]), {}
+        raw_prediction = np.clip(self.predict_raw(observation), -1, 1)
+        return np.float32([raw_prediction]), {}
     
-    def predict_raw(self, pop):
-        population = pop[0]
-        if population < self.x1:
-            return 0
-        elif self.x1 <= population < self.x2:
-            return self.y2 * (population - self.x1) / (self.x2 - self.x1)
+    def predict_raw(self, observation):
+        if observation < self.x1_pm1:
+            return -1
+        elif self.x1_pm1 <= observation < self.x2_pm1:
+            return (
+                -1 
+                + (
+                    (self.y2_pm1 + 1) 
+                    * (observation - self.x1_pm1) 
+                    / (self.x2_pm1 - self.x1_pm1)
+                ) # -1 + (y2 - - 1) * fraction
+            ),
         else:
-            return self.y2
+            return self.y2_pm1
 
     def predict_effort(self, state):
         return (self.predict(state) + 1) / 2
