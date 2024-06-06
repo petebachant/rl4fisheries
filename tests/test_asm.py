664965fc2dcc87e0382df5ecd6b3d1e4e53e37b3
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-06-06 18:54:22 +0000

    updated tests

--- tests/test_asm.py
+++ tests/test_asm.py
@@ -1,15 +1,12 @@
 # Confirm environment is correctly defined:
 from stable_baselines3.common.env_checker import check_env
-from rl4fisheries import Asm, Asm2o, AsmEsc, AsmEnv
-
-def test_Asm():
-    check_env(Asm(), warn=True)
-
-def test_Asm2o():
-    check_env(Asm2o(), warn=True)
+from rl4fisheries import AsmEnvEsc, AsmEnv, AsmCRLike
 
 def test_AsmEsc():
-    check_env(AsmEsc(), warn=True)
+    check_env(AsmEnvEsc(), warn=True)
 
 def test_AsmEnv():
     check_env(AsmEnv(), warn=True)
+
+def test_AsmCRLike():
+    check_env(AsmCRLike(), warn=True)
