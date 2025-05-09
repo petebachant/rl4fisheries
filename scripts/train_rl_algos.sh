d068669e783a3060fd56ab8188193d87c6a0fc93
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2025-05-09 16:08:47 +0000

    train bigger 2o networks too in bash script

--- scripts/train_rl_algos.sh
+++ scripts/train_rl_algos.sh
@@ -12,13 +12,16 @@ python hf_login.py
 
 # train
 python train.py -f ../hyperpars/noisy_obs/ppo_both_UM1.yml
+python train.py -f ../hyperpars/noisy_obs/ppo_both_UM1_deep.yml
 # python train.py -f ../hyperpars/noisy_obs/ppo_mwt_UM1.yml
 python train.py -f ../hyperpars/noisy_obs/ppo_biomass_UM1.yml
 
 python train.py -f ../hyperpars/noisy_obs/ppo_both_UM2.yml
+python train.py -f ../hyperpars/noisy_obs/ppo_both_UM2_deep.yml
 # python train.py -f ../hyperpars/noisy_obs/ppo_mwt_UM2.yml
 python train.py -f ../hyperpars/noisy_obs/ppo_biomass_UM2.yml
 
 python train.py -f ../hyperpars/noisy_obs/ppo_both_UM3.yml
+python train.py -f ../hyperpars/noisy_obs/ppo_both_UM3_deep.yml
 # python train.py -f ../hyperpars/noisy_obs/ppo_mwt_UM3.yml
 python train.py -f ../hyperpars/noisy_obs/ppo_biomass_UM3.yml
